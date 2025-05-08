5c9a947cab8ef8111f5f93f3911e643450178401
Prehistoricman <ezekieljames@hotmail.com>
2025-05-08 03:41:04 +0100

    Version 0.64

--- include/config.h
+++ include/config.h
@@ -3,7 +3,7 @@
 #define PROG_NAME "XBlast OS"
 
 //XBlast OS version number
-#define VERSION "0.63"
+#define VERSION "0.64"
 
 // selects between the supported video modes, see boot.h for enum listing those available
 //#define VIDEO_PREFERRED_MODE VIDEO_MODE_800x600
